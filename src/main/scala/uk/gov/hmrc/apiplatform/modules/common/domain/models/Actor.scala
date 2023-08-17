a34edc427fef2da131ac85a998824d48eea437da
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2023-08-17 07:45:00 +0100

    API-7247 - Expose ActorTypes so we can filter on them (#17)

--- src/main/scala/uk/gov/hmrc/apiplatform/modules/common/domain/models/Actor.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/modules/common/domain/models/Actor.scala
@@ -16,6 +16,9 @@
 
 package uk.gov.hmrc.apiplatform.modules.common.domain.models
 
+import play.api.libs.json.{Format, JsError, JsObject, JsResult, JsString, JsSuccess, JsValue, Json}
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.Actors.{AppCollaborator, GatekeeperUser, ScheduledJob}
+
 /** Actor refers to actors that triggered an event
   */
 sealed trait Actor
@@ -49,32 +52,78 @@ object Actors {
 
 }
 
+sealed abstract class ActorType(val description: String)
+
+object ActorType {
+
+  implicit val formatActorType = new Format[ActorType] {
+
+    override def writes(o: ActorType): JsValue =
+      Json.obj("description" -> o.description, "type" -> o.toString)
+
+    override def reads(json: JsValue): JsResult[ActorType] = {
+      (json match {
+        case JsString(text) => ActorTypes.fromString(text)
+        case JsObject(obj)  => obj.get("type").flatMap(_ match {
+            case JsString(t) => ActorTypes.fromString(t)
+            case _           => None
+          })
+        case _              => None
+      }).fold[JsResult[ActorType]](JsError(s"Cannot find actor Type"))(JsSuccess(_))
+    }
+  }
+}
+
+object ActorTypes {
+  case object COLLABORATOR extends ActorType("Application Collaborator")
+
+  case object GATEKEEPER extends ActorType("Gatekeeper User")
+
+  case object SCHEDULED_JOB extends ActorType("Scheduled Job")
+
+  case object UNKNOWN extends ActorType("Unknown")
+  private val ALL = Set(COLLABORATOR, GATEKEEPER, SCHEDULED_JOB, UNKNOWN)
+
+  /*
+   * Used for display purposes
+   */
+  private val lookupDescription: Map[String, ActorType] = ALL.map(at => at.description -> at).toMap
+
+  def fromDescription(text: String): Option[ActorType] = lookupDescription.get(text)
+
+  /*
+   * Used for Json only
+   */
+  private val lookupName: Map[String, ActorType] = ALL.map(at => at.toString -> at).toMap
+
+  def fromString(text: String): Option[ActorType] = lookupName.get(text)
+
+  def actorType(a: Actor): ActorType = a match {
+    case _: GatekeeperUser  => GATEKEEPER
+    case _: AppCollaborator => COLLABORATOR
+    case _: ScheduledJob    => SCHEDULED_JOB
+    case _                  => UNKNOWN
+  }
+
+}
+
 object Actor {
   import play.api.libs.json._
   import uk.gov.hmrc.play.json.Union
 
-  private sealed trait ActorType
-
-  private object ActorTypes {
-    case object COLLABORATOR  extends ActorType
-    case object GATEKEEPER    extends ActorType
-    case object SCHEDULED_JOB extends ActorType
-    case object UNKNOWN       extends ActorType
-  }
-
-  implicit val actorsCollaboratorWrites   = Json.writes[Actors.AppCollaborator]
-  implicit val actorsGatekeeerpUserWrites = Json.writes[Actors.GatekeeperUser]
-  implicit val actorsScheduledJobWrites   = Json.writes[Actors.ScheduledJob]
+  implicit val actorsCollaboratorWrites: OWrites[Actors.AppCollaborator]  = Json.writes[Actors.AppCollaborator]
+  implicit val actorsGatekeeperUserWrites: OWrites[Actors.GatekeeperUser] = Json.writes[Actors.GatekeeperUser]
+  implicit val actorsScheduledJobWrites: OWrites[Actors.ScheduledJob]     = Json.writes[Actors.ScheduledJob]
 
   import play.api.libs.functional.syntax._
   import play.api.libs.json.Reads._
 
   implicit val actorsCollaboratorReads: Reads[Actors.AppCollaborator]  =
     ((JsPath \ "id").read[String] or (JsPath \ "email").read[String]).map(s => Actors.AppCollaborator(LaxEmailAddress(s)))
-  implicit val actorsGatekeeerpUserReads: Reads[Actors.GatekeeperUser] = ((JsPath \ "id").read[String] or (JsPath \ "user").read[String]).map(Actors.GatekeeperUser(_))
+  implicit val actorsGatekeeperUserReads: Reads[Actors.GatekeeperUser] = ((JsPath \ "id").read[String] or (JsPath \ "user").read[String]).map(Actors.GatekeeperUser(_))
   implicit val actorsScheduledJobReads: Reads[Actors.ScheduledJob]     = ((JsPath \ "id").read[String] or (JsPath \ "jobId").read[String]).map(Actors.ScheduledJob(_))
 
-  implicit val actorsUnknownJF = Json.format[Actors.Unknown.type]
+  implicit val actorsUnknownJF: OFormat[Actors.Unknown.type] = Json.format[Actors.Unknown.type]
 
   implicit val formatNewStyleActor: OFormat[Actor] = Union.from[Actor]("actorType")
     .and[Actors.AppCollaborator](ActorTypes.COLLABORATOR.toString)
