712cfb95da28d3349f7b1299d31c1f4b3c348ce6
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-09-19 16:06:54 +0100

    APIS-6412 - Bump lib version

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -6,10 +6,9 @@ object LibraryDependencies {
   lazy val compileDependencies = Seq(
     "com.typesafe.play"       %% "play-json"                      % "2.9.2",
     "uk.gov.hmrc"             %% "play-json-union-formatter"      % "1.18.0-play-28",
-    "uk.gov.hmrc"             %% "api-platform-common-domain"     % "0.1.2",
+    "uk.gov.hmrc"             %% "api-platform-common-domain"     % "0.1.3",
     "com.github.t3hnar"       %% "scala-bcrypt"                   % "4.1",
-    "com.typesafe"             % "config"                         % "1.4.2",
-    "org.typelevel"           %% "cats-core"                      % "2.9.0"
+    "com.typesafe"             % "config"                         % "1.4.2"
   )
 
   lazy val testDependencies = Seq(
