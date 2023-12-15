9283f27bd60eb07e4db6a8de33c13ba091a66eff
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-12-15 14:45:19 +0000

    APIS-6481 - Bump common lib version

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -2,17 +2,20 @@ import sbt._
 
 object LibraryDependencies {
   def apply() = compileDependencies ++ testDependencies
+  
+  val commonDomainVersion = "0.10.0"
 
   lazy val compileDependencies = Seq(
-    "uk.gov.hmrc"             %% "api-platform-common-domain"     % "0.7.0",
+    "uk.gov.hmrc"             %% "api-platform-common-domain"     % commonDomainVersion,
     "com.github.t3hnar"       %% "scala-bcrypt"                   % "4.1",
     "com.typesafe"             % "config"                         % "1.4.2"
   )
 
   lazy val testDependencies = Seq(
-    "org.scalactic"           %% "scalactic"                      % "3.2.14",
-    "com.vladsch.flexmark"     % "flexmark-all"                   % "0.62.2",
-    "org.mockito"             %% "mockito-scala-scalatest"        % "1.17.29",
-    "org.scalatest"           %% "scalatest"                      % "3.2.17"
+    "org.scalactic"           %% "scalactic"                        % "3.2.14",
+    "com.vladsch.flexmark"     % "flexmark-all"                     % "0.62.2",
+    "org.mockito"             %% "mockito-scala-scalatest"          % "1.17.29",
+    "org.scalatest"           %% "scalatest"                        % "3.2.17",
+    "uk.gov.hmrc"             %% "api-platform-test-common-domain"  % commonDomainVersion
   ).map(_ % "test")
 }
