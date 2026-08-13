ed62dc9e152dffb2dbdb23521a83e7a9a7c2abb6
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-08-13 08:14:03 +0100

    API-provided - POC to show breaking bonds of libs

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -1,17 +1,15 @@
 import sbt._
 
 object LibraryDependencies {
-  def apply(scalaVersion: String) = compileDependencies(scalaVersion) ++ testDependencies(scalaVersion)
-
   val commonDomainVersion = "1.3.0"
   val monocleVersion = "3.1.0"
 
   def applicationDomain(scalaVersion: String) = compileDependencies(scalaVersion) ++ testDependencies(scalaVersion).map(_ % "test")
 
-  def root(scalaVersion: String) = compileDependencies(scalaVersion) ++ testDependencies(scalaVersion)
+  def root(scalaVersion: String) = compileDependencies(scalaVersion) ++ testDependencies(scalaVersion, true)
 
   def compileDependencies(scalaVersion: String) = Seq(
-    "uk.gov.hmrc"             %% "api-platform-common-domain"     % commonDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-common-domain"     % commonDomainVersion % "provided",
     "com.typesafe"             % "config"                         % "1.4.2",
     "commons-validator"        % "commons-validator"              % "1.9.0",
     "dev.optics"              %% "monocle-core"                   % monocleVersion,
@@ -23,9 +21,13 @@ object LibraryDependencies {
       }
     )
 
-  def testDependencies(scalaVersion: String) = Seq(
+  def testDependencies(scalaVersion: String, provided: Boolean = false) = Seq(
     "com.vladsch.flexmark"     % "flexmark-all"                         % "0.64.8",
-    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures"  % commonDomainVersion,
     "org.scalatest"           %% "scalatest"                            % "3.2.19"
-    )
+  ) ++ Seq(
+    if(provided)
+      "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures"  % commonDomainVersion % "provided"
+    else 
+      "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures"  % commonDomainVersion
+  )
 }
